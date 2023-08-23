6975c567453457118a9d0804ea4de408f6cf26f9
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-23 10:52:13 +0530

    configured store

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { makeServer } from "./server";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
 
 // Call make Server
 makeServer();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
