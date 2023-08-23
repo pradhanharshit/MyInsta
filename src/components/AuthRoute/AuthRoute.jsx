eb4ea7a205a5c70f19ad9ab46d5432daf64b8730
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-23 12:38:36 +0530

    fix

--- src/components/AuthRoute/AuthRoute.jsx
+++ src/components/AuthRoute/AuthRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AuthRoute = () => {
-  const { authToken } = useSelector((state) => state.authToken);
+  const { authToken } = useSelector((state) => state.auth);
   return authToken !== null ? <Navigate to="/" replace={true} /> : <Outlet />;
 };
 
