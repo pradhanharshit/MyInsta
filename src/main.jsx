79539c4107744626bbadcd72bcd58f1d6fa78452
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-23 09:19:10 +0530

    configured server

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { makeServer } from "./server";
 
-import server from "./backend/Server/server.js";
-
-server();
+// Call make Server
+makeServer();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
