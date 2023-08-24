cd73b013cf98fef27a50526f93dbc44934aa3ea1
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-25 05:16:34 +0530

    Added theme context

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { makeServer } from "./server";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import Router from "./Router/Router.jsx";
+import { ThemeProvider } from "./context/themeContext";
 
 // Call make Server
 makeServer();
@@ -12,7 +13,9 @@ makeServer();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router />
+      <ThemeProvider>
+        <Router />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
