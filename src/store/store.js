d9470ae44db78f9bc18e83c272bd37c906026398
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-26 00:49:20 +0530

    feat: users & posts slice added

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
+import userSlice from "./userSlice";
+import postSlice from "./postSlice";
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
+    users: userSlice,
+    posts: postSlice,
   },
 });
 
