d436496181f9b3d8675e705de45fba85025bcf72
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-26 23:21:27 +0530

    feat: homefeed showing all posts

--- src/components/HomeFeed/HomeFeed.jsx
+++ src/components/HomeFeed/HomeFeed.jsx
@@ -1,15 +1,29 @@
-import PostCard from "../../components/PostCard/PostCard"
-import Filter from "../../components/Filter/Filter"
-import NewPostCard from "../NewPostCard/NewPostCard"
+import PostCard from "../../components/PostCard/PostCard";
+import Filter from "../../components/Filter/Filter";
+import NewPostCard from "../NewPostCard/NewPostCard";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllPosts } from "../../services/postService";
 
 const HomeFeed = () => {
+  const { newPostAdded, allPosts } = useSelector((state) => state.posts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllPosts());
+  }, [newPostAdded]);
+
   return (
     <>
-        <Filter />
-        <NewPostCard />
-        <PostCard />
+      <Filter />
+      <NewPostCard />
+      <div>
+        {allPosts.map((post) => {
+          return <PostCard key={post.id} postData={post} />;
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomeFeed
+export default HomeFeed;
