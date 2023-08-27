f11b772b3d448037eda047f2031394a580402ea6
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-27 16:38:31 +0530

    fix: indent fix

--- src/components/ConnectUserCard/ConnectUserCard.jsx
+++ src/components/ConnectUserCard/ConnectUserCard.jsx
@@ -1,61 +1,97 @@
-import Card from "../Card/Card"
+import Card from "../Card/Card";
 
 const ConnectUserCard = () => {
   return (
     <>
-    <Card>
+      <Card>
         <div className="flex justify-between items-center">
-            <img className="rounded-full h-[50px] w-[50px]" src="Profile.png" alt="" />
-            <div className="text-center">
-                <p className="text-lg">Name</p>
-                <p className="text-sm text-gray-700">UserName</p>
-            </div>
-            <div className="text-lg font-semibold">
-            <button to="my-profile/Liked" className="px-3 py-2 rounded-3xl bg-blue-400 text-white">follow</button>
-            </div>
+          <img
+            className="rounded-full h-[50px] w-[50px]"
+            src="Profile.png"
+            alt=""
+          />
+          <div className="text-center">
+            <p className="text-lg">Name</p>
+            <p className="text-sm text-gray-700">UserName</p>
+          </div>
+          <div className="text-lg font-semibold">
+            <button
+              to="my-profile/Liked"
+              className="px-3 py-2 rounded-3xl bg-blue-400 text-white"
+            >
+              follow
+            </button>
+          </div>
         </div>
-    </Card>
+      </Card>
 
-    <Card>
+      <Card>
         <div className="flex justify-between items-center">
-            <img className="rounded-full h-[50px] w-[50px]" src="Profile.png" alt="" />
-            <div className="text-center">
-                <p className="text-lg">Name</p>
-                <p className="text-sm text-gray-700">UserName</p>
-            </div>
-            <div className="text-lg font-semibold">
-            <button to="my-profile/Liked" className="px-3 py-2 rounded-3xl bg-blue-400 text-white">follow</button>
-            </div>
+          <img
+            className="rounded-full h-[50px] w-[50px]"
+            src="Profile.png"
+            alt=""
+          />
+          <div className="text-center">
+            <p className="text-lg">Name</p>
+            <p className="text-sm text-gray-700">UserName</p>
+          </div>
+          <div className="text-lg font-semibold">
+            <button
+              to="my-profile/Liked"
+              className="px-3 py-2 rounded-3xl bg-blue-400 text-white"
+            >
+              follow
+            </button>
+          </div>
         </div>
-    </Card>
+      </Card>
 
-    <Card>
+      <Card>
         <div className="flex justify-between items-center">
-            <img className="rounded-full h-[50px] w-[50px]" src="Profile.png" alt="" />
-            <div className="text-center">
-                <p className="text-lg">Name</p>
-                <p className="text-sm text-gray-700">UserName</p>
-            </div>
-            <div className="text-lg font-semibold">
-            <button to="my-profile/Liked" className="px-3 py-2 rounded-3xl bg-blue-400 text-white">follow</button>
-            </div>
+          <img
+            className="rounded-full h-[50px] w-[50px]"
+            src="Profile.png"
+            alt=""
+          />
+          <div className="text-center">
+            <p className="text-lg">Name</p>
+            <p className="text-sm text-gray-700">UserName</p>
+          </div>
+          <div className="text-lg font-semibold">
+            <button
+              to="my-profile/Liked"
+              className="px-3 py-2 rounded-3xl bg-blue-400 text-white"
+            >
+              follow
+            </button>
+          </div>
         </div>
-    </Card>
+      </Card>
 
-    <Card>
+      <Card>
         <div className="flex justify-between items-center">
-            <img className="rounded-full h-[50px] w-[50px]" src="Profile.png" alt="" />
-            <div className="text-center">
-                <p className="text-lg">Name</p>
-                <p className="text-sm text-gray-700">UserName</p>
-            </div>
-            <div className="text-lg font-semibold">
-            <button to="my-profile/Liked" className="px-3 py-2 rounded-3xl bg-blue-400 text-white">follow</button>
-            </div>
+          <img
+            className="rounded-full h-[50px] w-[50px]"
+            src="Profile.png"
+            alt=""
+          />
+          <div className="text-center">
+            <p className="text-lg">Name</p>
+            <p className="text-sm text-gray-700">UserName</p>
+          </div>
+          <div className="text-lg font-semibold">
+            <button
+              to="my-profile/Liked"
+              className="px-3 py-2 rounded-3xl bg-blue-400 text-white"
+            >
+              follow
+            </button>
+          </div>
         </div>
-    </Card>
+      </Card>
     </>
-  )
-}
+  );
+};
 
-export default ConnectUserCard
+export default ConnectUserCard;
