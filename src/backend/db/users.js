7f8abe2a7d1aeb43413c95770e6eb29866ee4d0b
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-27 16:37:42 +0530

    feat: hardcoded users

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,4 +1,3 @@
-import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
 /**
  * User Database can be added here.
@@ -7,11 +6,91 @@ import { formatDate } from "../utils/authUtils";
 
 export const users = [
   {
-    _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "test@gmail.com",
+    _id: "531585dd-f521-4cb3-a3e9-415e3fec4cc4",
+    firstName: "Harshit",
+    lastName: "Pradhan",
+    username: "test@harshit",
     password: "test",
+    bio: "Hy!! This is Harshit Pradhan",
+    profilePicture:
+      "https://i.pinimg.com/736x/0d/64/14/0d64145c56a53e2886274bf99a10d7d0.jpg",
+    coverPicture:
+      "https://i.pinimg.com/564x/2a/53/a9/2a53a919196eadda665db1ad0e075e50.jpg",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "caac2a91-4e40-46ea-813c-ddcbf777be8e",
+    firstName: "User",
+    lastName: "2",
+    username: "test@2",
+    password: "test2",
+    bio: "I'm user 2",
+    profilePicture:
+      "https://i.pinimg.com/564x/84/fd/cd/84fdcdda87fd125115132bfc65b83202.jpg",
+    coverPicture:
+      "https://i.pinimg.com/564x/2a/53/a9/2a53a919196eadda665db1ad0e075e50.jpg",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "7372bf09-f39e-40e2-8de3-94c8510efd67",
+    firstName: "User",
+    lastName: "3",
+    username: "test@3",
+    password: "test3",
+    bio: "I'm user 3",
+    profilePicture:
+      "https://i.pinimg.com/564x/81/7d/2c/817d2ccf7b502274f0c0dbaa616a5eee.jpg",
+    coverPicture:
+      "https://i.pinimg.com/564x/2a/53/a9/2a53a919196eadda665db1ad0e075e50.jpg",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "7a1da952-5b0e-436e-9eca-2e58b50523e7",
+    firstName: "User",
+    lastName: "4",
+    username: "test@4",
+    password: "test4",
+    bio: "I'm user 4",
+    profilePicture:
+      "https://i.pinimg.com/564x/98/58/ed/9858edafb6b4d9ebb91c559fbda234f7.jpg",
+    coverPicture:
+      "https://i.pinimg.com/564x/2a/53/a9/2a53a919196eadda665db1ad0e075e50.jpg",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "e95b8297-5327-4758-b3ee-8dcdecbdf5d8",
+    firstName: "User",
+    lastName: "5",
+    username: "test@5",
+    password: "test5",
+    bio: "I'm user 5",
+    profilePicture:
+      "https://i.pinimg.com/564x/03/41/55/034155df399f5103fa24efaa91652256.jpg",
+    coverPicture:
+      "https://i.pinimg.com/564x/2a/53/a9/2a53a919196eadda665db1ad0e075e50.jpg",
+    createdAt: formatDate(),
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: "692d2f62-cadb-44c6-b284-9235267702bb",
+    firstName: "User",
+    lastName: "6",
+    username: "test@6",
+    password: "test6",
+    bio: "I'm user 6",
+    profilePicture:
+      "https://i.pinimg.com/564x/49/17/ff/4917ff52aaf6723efd2ef17706a4eaee.jpg",
+    coverPicture:
+      "https://i.pinimg.com/564x/2a/53/a9/2a53a919196eadda665db1ad0e075e50.jpg",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
