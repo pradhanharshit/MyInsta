fac08692e6792f5b0edbd7f1eee3a12273d1abcb
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-27 16:44:11 +0530

    feat: new post & post update states  & reducers

--- src/store/postSlice.js
+++ src/store/postSlice.js
@@ -4,13 +4,26 @@ import { getAllPosts } from "../services/postService";
 const postSlice = createSlice({
   name: "posts",
   initialState: {
-    allposts: [],
+    allPosts: [],
+    newPostAdded: true,
+    postUpdated: true,
+  },
+  reducers: {
+    addedNewPost(state) {
+      state.newPostAdded = !state.newPostAdded;
+    },
+    onPostUpdate(state) {
+      // console.log("called");
+      state.postUpdated = !state.postUpdated;
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(getAllPosts.fulfilled, (state, action) => {
-      console.log(action.payload);
+      state.allPosts = action.payload.data.posts;
+      // console.log(state.allPosts);
     });
   },
 });
 
+export const { addedNewPost, onPostUpdate } = postSlice.actions;
 export default postSlice.reducer;
