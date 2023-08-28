c9a159b73c108caf54a78e1286bfbc1437dbce8b
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-28 17:15:59 +0530

    feat: edit & delete comment service

--- src/services/commentsService.js
+++ src/services/commentsService.js
@@ -26,3 +26,42 @@ export const getCommentsHandler = async (postId) => {
     console.log(error);
   }
 };
+
+export const editCommentHandler = async (
+  commentData,
+  postId,
+  commentId,
+  authToken
+) => {
+  try {
+    // console.log("new", newcomment);
+    const res = await axios.post(
+      `api/comments/edit/${postId}/${commentId}`,
+      { commentData },
+      {
+        headers: {
+          authorization: authToken,
+        },
+      }
+    );
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deleteCommentHandler = async (postId, commentId, authToken) => {
+  try {
+    await axios.post(
+      `api/comments/delete/${postId}/${commentId}`,
+      {},
+      {
+        headers: {
+          authorization: authToken,
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
