23af0f7a4624353faead4561e2eddc16f3ee79d8
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-31 08:49:29 +0530

    feat: bookmark page rendering

--- src/pages/Bookmarks/Bookmarks.jsx
+++ src/pages/Bookmarks/Bookmarks.jsx
@@ -1,9 +1,27 @@
-
+import { useSelector } from "react-redux";
+import { useTheme } from "../../context/ThemeContext";
+import PostCard from "../../components/PostCard/PostCard";
 
 const Bookmarks = () => {
-  return (
-    <h1 className="font-serif font-bold text-2xl m-8">Bookmarked Posts</h1>
-  )
-}
+  const { themeObject } = useTheme();
+  const { bookmarkedPosts } = useSelector((state) => state.posts);
 
-export default Bookmarks
+  return (
+    <div>
+      <div className="text-center">
+        <h1
+          className="font-serif font-bold text-2xl m-8"
+          style={{ color: themeObject.text }}
+        >
+          Bookmarked Posts
+        </h1>
+      </div>
+      {bookmarkedPosts.length > 0 &&
+        bookmarkedPosts.map((markedPost) => {
+          return <PostCard key={markedPost._id} postData={markedPost} />;
+        })}
+    </div>
+  );
+};
+
+export default Bookmarks;
