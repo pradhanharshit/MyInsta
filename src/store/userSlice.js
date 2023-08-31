1ba5cb3cae5aa770b23430067d5567b2f02ca78c
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-31 08:52:46 +0530

    feat: added thunks & reducers

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -1,20 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { getAllUsers, getOwnerData } from "../services/userService";
+import {
+  getAllUsers,
+  getOwnerData,
+  getUserData,
+} from "../services/userService";
 
 const userSlice = createSlice({
   name: "user",
   initialState: {
     ownerData: {},
+    userData: {},
     allUsers: [],
+    currentId: "",
+  },
+  reducers: {
+    changeCurrentId(state, action) {
+      state.currentId = action.payload;
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(getOwnerData.fulfilled, (state, action) => {
-      state.ownerData = action.payload.data.user;
+      state.ownerData = JSON.parse(JSON.stringify(action.payload));
+    });
+    builder.addCase(getUserData.fulfilled, (state, action) => {
+      state.userData = action.payload.data.user;
     });
     builder.addCase(getAllUsers.fulfilled, (state, action) => {
-      console.log(action.payload);
+      state.allUsers = action.payload;
     });
   },
 });
 
+export const { changeCurrentId } = userSlice.actions;
 export default userSlice.reducer;
