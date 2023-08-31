c0e0cbfdffa5048d876fd04f0a90af60857c3e97
Harshit Pradhan <hpradhan020@gmail.com>
2023-08-31 09:32:33 +0530

    feat: explore feed rendered

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import { useTheme } from "../../context/ThemeContext";
+import PostCard from "../../components/PostCard/PostCard";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  const { themeObject } = useTheme();
+  const { exploreFeedPosts } = useSelector((state) => state.posts);
+  return (
+    <div>
+      <div className="text-center">
+        <h1
+          className="font-serif font-bold text-2xl m-8"
+          style={{ color: themeObject.text }}
+        >
+          Explore
+        </h1>
+      </div>
+      {exploreFeedPosts.map((post) => {
+        return <PostCard key={post._id} postData={post} />;
+      })}
+    </div>
+  );
 };
 
 export default Explore;
