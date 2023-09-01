7d5a1a5fcba770f1e9af4e39ae2e83dcf5c15597
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-01 13:25:30 +0530

    connect people page

--- src/pages/ConnectPeople/ConnectPeople.jsx
+++ src/pages/ConnectPeople/ConnectPeople.jsx
@@ -1,14 +1,34 @@
-import ConnectUserCard from "../../components/ConnectUserCard/ConnectUserCard"
-
+import { useSelector } from "react-redux";
+import ConnectUserCard from "../../components/ConnectUserCard/ConnectUserCard";
+import { useTheme } from "../../context/ThemeContext";
 
 const ConnectPeople = () => {
+  const { themeObject } = useTheme();
+  const { allUsers, ownerData } = useSelector((store) => store.users);
   return (
     <>
-      <h1 className="font-serif font-bold text-2xl m-8">Connect</h1>
-      <ConnectUserCard />
+      <div className="text-center">
+        <h1
+          className="font-serif font-bold text-2xl m-8"
+          style={{ color: themeObject.text }}
+        >
+          Connect
+        </h1>
+      </div>
+
+      {allUsers
+        ?.filter(
+          (userdata) =>
+            userdata?._id !== ownerData?._id &&
+            ownerData?.following?.some(
+              (followingUser) => followingUser?._id === userdata?._id
+            ) === false
+        )
+        .map((userdata) => {
+          return <ConnectUserCard userdata={userdata} key={userdata.id} />;
+        })}
     </>
+  );
+};
 
-  )
-}
-
-export default ConnectPeople
+export default ConnectPeople;
