cfbd83b132f340b9c05a9c093dde87f44023cade
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-01 13:22:51 +0530

    feat: showing dynamic data of users

--- src/components/ConnectUserCard/ConnectUserCard.jsx
+++ src/components/ConnectUserCard/ConnectUserCard.jsx
@@ -1,84 +1,27 @@
+/* eslint-disable react/prop-types */
 import Card from "../Card/Card";
+import { useTheme } from "../../context/ThemeContext";
 
-const ConnectUserCard = () => {
+const ConnectUserCard = ({ userdata }) => {
+  const { themeObject } = useTheme();
   return (
     <>
       <Card>
         <div className="flex justify-between items-center">
           <img
             className="rounded-full h-[50px] w-[50px]"
-            src="Profile.png"
-            alt=""
+            src={userdata.profilePicture}
           />
           <div className="text-center">
-            <p className="text-lg">Name</p>
-            <p className="text-sm text-gray-700">UserName</p>
-          </div>
-          <div className="text-lg font-semibold">
-            <button
-              to="my-profile/Liked"
-              className="px-3 py-2 rounded-3xl bg-blue-400 text-white"
+            <p className="text-lg" style={{ color: themeObject.text }}>
+              {userdata.firstName} {userdata.lastName}
+            </p>
+            <p
+              className="text-sm text-gray-700"
+              style={{ color: themeObject.text }}
             >
-              follow
-            </button>
-          </div>
-        </div>
-      </Card>
-
-      <Card>
-        <div className="flex justify-between items-center">
-          <img
-            className="rounded-full h-[50px] w-[50px]"
-            src="Profile.png"
-            alt=""
-          />
-          <div className="text-center">
-            <p className="text-lg">Name</p>
-            <p className="text-sm text-gray-700">UserName</p>
-          </div>
-          <div className="text-lg font-semibold">
-            <button
-              to="my-profile/Liked"
-              className="px-3 py-2 rounded-3xl bg-blue-400 text-white"
-            >
-              follow
-            </button>
-          </div>
-        </div>
-      </Card>
-
-      <Card>
-        <div className="flex justify-between items-center">
-          <img
-            className="rounded-full h-[50px] w-[50px]"
-            src="Profile.png"
-            alt=""
-          />
-          <div className="text-center">
-            <p className="text-lg">Name</p>
-            <p className="text-sm text-gray-700">UserName</p>
-          </div>
-          <div className="text-lg font-semibold">
-            <button
-              to="my-profile/Liked"
-              className="px-3 py-2 rounded-3xl bg-blue-400 text-white"
-            >
-              follow
-            </button>
-          </div>
-        </div>
-      </Card>
-
-      <Card>
-        <div className="flex justify-between items-center">
-          <img
-            className="rounded-full h-[50px] w-[50px]"
-            src="Profile.png"
-            alt=""
-          />
-          <div className="text-center">
-            <p className="text-lg">Name</p>
-            <p className="text-sm text-gray-700">UserName</p>
+              {userdata.username}
+            </p>
           </div>
           <div className="text-lg font-semibold">
             <button
