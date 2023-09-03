7691b9718439d802822e0251b5ba71ecb3527e00
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-03 14:40:31 +0530

    feat: dynamic home profile data

--- src/components/HomeProfile/HomeProfile.jsx
+++ src/components/HomeProfile/HomeProfile.jsx
@@ -1,39 +1,64 @@
-import "./HomeProfile.css"
-import { Link } from "react-router-dom"
+import { useSelector } from "react-redux";
+import "./HomeProfile.css";
+import { Link } from "react-router-dom";
+import { useTheme } from "../../context/ThemeContext";
 
 const HomeProfile = () => {
+  const { themeObject } = useTheme();
+  const { ownerData } = useSelector((state) => state.users);
+  // console.log(ownerData);
   return (
-    <div className="homeprofile-container w-[21.5rem] h-[30rem] bg-gray-200 rounded-3xl">
+    <div
+      className="homeprofile-container w-[21.5rem] h-fit rounded-3xl"
+      style={{ backgroundColor: themeObject.secondary }}
+    >
+      <img
+        className="homeprofile-cover-img h-[40%] w-[100%] rounded-t-3xl"
+        src={ownerData?.coverPicture}
+      />
 
-    <img className="homeprofile-cover-img h-[40%] w-[100%] rounded-t-3xl" src="CoverImage.png" alt="" />
+      <img
+        className="homeprofile-img rounded-full h-[80px] w-[80px]"
+        src={ownerData?.profilePicture}
+      />
 
-    <img className="homeprofile-img rounded-full h-[80px] w-[80px]" src="Profile.png" alt="" />
+      <div className="flex flex-col mt-9 text-center">
+        <span className="text-xl">
+          {ownerData?.firstName} {ownerData?.lastName}
+        </span>
+        <span className="text-sm text-gray-700">{ownerData?.username}</span>
+      </div>
 
-    <div className="flex flex-col mt-9 text-center">
-    <span className="text-xl">Name</span>
-    <span className="text-sm text-gray-700">UserName</span>
-    </div>
+      <div className="homeprofile-bio pl-5 pr-5 mt-5 text-center text-gray-700">
+        {ownerData?.bio}
+      </div>
 
-    <div className="homeprofile-bio pl-5 pr-5 mt-5 text-center text-gray-700">Hy! This is Harshit Pradhan. Software Developer</div>
-
-    <div className="mt-8 flex justify-around pl-8 pr-8 text-base">
+      <div className="mt-8 flex justify-around pl-8 pr-8 text-base">
         <div className="followers flex space-x-1">
-            <span>0</span>
-            <span className="font-bold text-blue-400">Followers</span>
+          <span style={{ color: themeObject.text }}>
+            {ownerData?.followers?.length}
+          </span>
+          <span className="font-bold text-blue-400">Followers</span>
         </div>
         <div className="sp-ln"></div>
         <div className="following flex space-x-1">
-            <span>0</span>
-            <span className="font-bold text-blue-400">Following</span>
+          <span style={{ color: themeObject.text }}>
+            {ownerData?.following?.length}
+          </span>
+          <span className="font-bold text-blue-400">Following</span>
         </div>
-    </div>
+      </div>
 
-    <div className="mt-5 mb-8 text-center text-lg font-semibold">
-        <Link to="my-profile" className="px-3 py-2 rounded-3xl bg-blue-400 text-white">My Profile</Link>
+      <div className="mt-5 mb-8 text-center text-lg font-semibold">
+        <Link
+          to="my-profile"
+          className="px-3 py-2 rounded-3xl bg-blue-400 text-white"
+        >
+          My Profile
+        </Link>
+      </div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default HomeProfile
+export default HomeProfile;
