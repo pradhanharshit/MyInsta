594f96116b8361ada22f480365e135c616ca4408
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-03 14:37:53 +0530

    feat: made myprofile link dynamic

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -1,26 +1,35 @@
-import {HomeIcon, BookmarkIcon, UsersIcon, MoonIcon, ArrowRightOnRectangleIcon, UserIcon} from "@heroicons/react/20/solid"
+import {
+  HomeIcon,
+  BookmarkIcon,
+  UsersIcon,
+  MoonIcon,
+  ArrowRightOnRectangleIcon,
+  UserIcon,
+} from "@heroicons/react/20/solid";
 import "./BottomBar.css";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const BottomBar = () => {
+  const { user } = useSelector((state) => state.auth);
   return (
     <div className="bottombar">
-        <NavLink to="/">
+      <NavLink to="/">
         <HomeIcon className="h-[30px] w-[30px]"></HomeIcon>
-        </NavLink>
-        <NavLink to="bookmarks">
+      </NavLink>
+      <NavLink to="bookmarks">
         <BookmarkIcon className="h-[30px] w-[30px]"></BookmarkIcon>
-        </NavLink>
-        <NavLink to="connect-people">
+      </NavLink>
+      <NavLink to="connect-people">
         <UsersIcon className="h-[30px] w-[30px]"></UsersIcon>
-        </NavLink>
-        <NavLink to="my-profile">
+      </NavLink>
+      <NavLink to={`/profile/${user.username}`}>
         <UserIcon className="h-[30px] w-[30px]"></UserIcon>
-        </NavLink>
-        <MoonIcon className="h-[30px] w-[30px]"></MoonIcon>
-        <ArrowRightOnRectangleIcon className="h-[30px] w-[30px]"></ArrowRightOnRectangleIcon>
+      </NavLink>
+      <MoonIcon className="h-[30px] w-[30px]"></MoonIcon>
+      <ArrowRightOnRectangleIcon className="h-[30px] w-[30px]"></ArrowRightOnRectangleIcon>
     </div>
-  )
-}
+  );
+};
 
-export default BottomBar
+export default BottomBar;
