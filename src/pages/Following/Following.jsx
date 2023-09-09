4e421aba211f2b86d2310d54a3b155e2d7780915
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-09 15:05:13 +0530

    feat: showing following data

--- src/pages/Following/Following.jsx
+++ src/pages/Following/Following.jsx
@@ -1,8 +1,15 @@
+import { useSelector } from "react-redux";
+import ConnectUserCard from "../../components/ConnectUserCard/ConnectUserCard";
 
 const Following = () => {
+  const { userData } = useSelector((state) => state.users);
   return (
-    <div>Following</div>
-  )
-}
+    <>
+      {userData?.following?.map((following) => {
+        return <ConnectUserCard key={following?._id} userdata={following} />;
+      })}
+    </>
+  );
+};
 
-export default Following
+export default Following;
