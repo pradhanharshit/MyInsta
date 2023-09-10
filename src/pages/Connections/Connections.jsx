a3429a4cb9ab914d30b35a2d1d9d6740aaf51c46
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-10 22:29:59 +0530

    feat: connections page

--- src/pages/Connections/Connections.jsx
+++ src/pages/Connections/Connections.jsx
@@ -1,9 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
+import { useTheme } from "../../context/ThemeContext";
+import "./Connections.css";
 
 const Connections = () => {
+  const { themeObject } = useTheme();
   return (
     <>
-      <div>Connections</div>
+      <div className="text-center">
+        <h1
+          className="font-serif font-bold text-2xl m-8"
+          style={{ color: themeObject.text }}
+        >
+          Connections
+        </h1>
+      </div>
+      <div
+        className="w-[80%] flex justify-around"
+        style={{ color: themeObject.text }}
+      >
+        <NavLink to="followers" className="connection font-serif">
+          Followers
+        </NavLink>
+        <NavLink to="following" className="connection font-serif">
+          Following
+        </NavLink>
+      </div>
       <Outlet />
     </>
   );
