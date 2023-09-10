167b4dab32ca876ef61e0037ba8078ef4e5ef296
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-10 22:00:14 +0530

    feat: new post modal responsive

--- src/components/BottomBar/BottomBar.jsx
+++ src/components/BottomBar/BottomBar.jsx
@@ -12,10 +12,16 @@ import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useTheme } from "../../context/ThemeContext";
 import { logout } from "../../store/authSlice";
+import { RocketLaunchIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import NewPostModal from "../NewPostModal/NewPostModal";
 
 const BottomBar = () => {
   const { user } = useSelector((state) => state.auth);
   const { themeObject, theme, toggleThemeHandler } = useTheme();
+
+  const [postModalOpen, setPostModalOpen] = useState(false);
+
   const dispatch = useDispatch();
   return (
     <div
@@ -34,6 +40,15 @@ const BottomBar = () => {
       <NavLink to={`/${user?.username}`}>
         <UserIcon className="h-[30px] w-[30px]" />
       </NavLink>
+      <div
+        className="new-post p-3 rounded-full bg-blue-400"
+        onClick={() => {
+          setPostModalOpen(true);
+        }}
+      >
+        <RocketLaunchIcon className="h-[30px] w-[30px] text-white" />
+      </div>
+      {postModalOpen && <NewPostModal setPostModalOpen={setPostModalOpen} />}
       <div
         className="flex space-x-3 items-center rounded-2xl"
         onClick={() => {
