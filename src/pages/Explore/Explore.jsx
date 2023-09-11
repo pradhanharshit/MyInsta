1318093fd8cfb1618012d83640fb9d158602433f
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-11 21:05:19 +0530

    feat: added infinite scrolling

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,10 +1,42 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useTheme } from "../../context/ThemeContext";
 import PostCard from "../../components/PostCard/PostCard";
+import { useEffect } from "react";
+import { getPagedPosts } from "../../services/postService";
+import { setPageNum } from "../../store/postSlice";
+import { useRef } from "react";
 
 const Explore = () => {
   const { themeObject } = useTheme();
-  const { exploreFeedPosts } = useSelector((state) => state.posts);
+  const { pagedPosts, pagedPostStatus, pageNum } = useSelector(
+    (state) => state.posts
+  );
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPagedPosts({ pageNum }));
+  }, [pageNum]);
+
+  const loader = useRef(null);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        dispatch(setPageNum());
+      }
+    });
+
+    if (loader.current) {
+      observer.observe(loader.current);
+    }
+
+    return () => {
+      if (loader.current) {
+        observer.unobserve(loader.current);
+      }
+    };
+  }, []);
+
   return (
     <div>
       <div className="text-center">
@@ -15,9 +47,18 @@ const Explore = () => {
           Explore
         </h1>
       </div>
-      {exploreFeedPosts.map((post) => {
+      {pagedPosts?.map((post) => {
         return <PostCard key={post._id} postData={post} />;
       })}
+      <div ref={loader} className="text-gray-50 text-3xl pb-2" />
+      {pagedPostStatus === "pending" && (
+        <div
+          className="text-3xl text-center"
+          style={{ color: themeObject.text }}
+        >
+          Loading...
+        </div>
+      )}
     </div>
   );
 };
