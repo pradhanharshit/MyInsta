65cf31beddab55501f6f1b144b5812ac2b9000bf
Harshit Pradhan <hpradhan020@gmail.com>
2023-09-11 16:29:00 +0530

    feat: sorting home feed filter

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,33 +1,74 @@
-import {SparklesIcon} from "@heroicons/react/24/outline"
-import { useState } from "react"
-import { ClickOutHandler } from "react-clickout-ts"
+import { SparklesIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { ClickOutHandler } from "react-clickout-ts";
+import { useTheme } from "../../context/ThemeContext";
 
-const Filter = () => {
+const Filter = ({ sort, setSort }) => {
   const [openFilter, setOpenFilter] = useState(false);
+  const { themeObject } = useTheme();
 
   return (
-      <>
-        <div className="flex justify-end items-center mt-[1rem] w-[85%]">
+    <>
+      <div className="flex justify-end items-center mt-[1rem] w-[85%]">
         <div>
-        <SparklesIcon className="h-8 w-8 stroke-blue-400" onClick={() => {
-          setOpenFilter(!openFilter)
-        }}/>
-        <ClickOutHandler onClickOut={() => setOpenFilter(false)}>
-          <div className="relative">
-            {openFilter && 
-            <div className="absolute right-0 bg-blue-400 text-white p-2 rounded-2xl">
-              <button>clear</button>
-              <button>recent</button>
-              <button>trending</button>
+          <SparklesIcon
+            className="h-8 w-8 stroke-blue-400"
+            onClick={() => {
+              setOpenFilter(!openFilter);
+            }}
+          />
+          <ClickOutHandler onClickOut={() => setOpenFilter(false)}>
+            <div className="relative">
+              {openFilter && (
+                <div
+                  className="absolute right-0 border-2 border-blue-400 p-2 rounded-2xl"
+                  style={{
+                    backgroundColor: themeObject.secondary,
+                    color: themeObject.text,
+                  }}
+                >
+                  <button
+                    className={`${sort === "recent" ? "underline" : null} mt-1`}
+                    onClick={() => {
+                      setSort("recent");
+                    }}
+                  >
+                    Recent
+                  </button>
+                  <button
+                    className={`${sort === "older" ? "underline" : null} mt-1`}
+                    onClick={() => {
+                      setSort("older");
+                    }}
+                  >
+                    Older
+                  </button>
+                  <button
+                    className={`${
+                      sort === "trending" ? "underline" : null
+                    } mt-1`}
+                    onClick={() => {
+                      setSort("trending");
+                    }}
+                  >
+                    Trending
+                  </button>
+                  <button
+                    className="underline mt-2 text-sm"
+                    onClick={() => {
+                      setSort("");
+                    }}
+                  >
+                    Clear
+                  </button>
+                </div>
+              )}
             </div>
-            }
-          </div>
-        </ClickOutHandler>
+          </ClickOutHandler>
         </div>
-        </div>
-        
-      </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Filter
+export default Filter;
--- src/components/HomeFeed/HomeFeed.jsx
+++ src/components/HomeFeed/HomeFeed.jsx
@@ -2,27 +2,68 @@ import PostCard from "../../components/PostCard/PostCard";
 import Filter from "../../components/Filter/Filter";
 import NewPostCard from "../NewPostCard/NewPostCard";
 import { useSelector } from "react-redux";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const HomeFeed = () => {
   const { homeFeedPosts } = useSelector((state) => state.posts);
   const { ownerData } = useSelector((state) => state.users);
+  const [sort, setSort] = useState("recent");
+  const [homeFeedSort, setHomeFeedSort] = useState([]);
 
-  let filteredHomeFeed = JSON.parse(JSON.stringify(homeFeedPosts));
+  useEffect(() => {
+    sortFeed();
+  }, [sort, homeFeedPosts]);
 
-  filteredHomeFeed = filteredHomeFeed.filter(
-    (post) =>
-      ownerData?.following.some((user) => user._id === post.user_id) ||
-      post.user_id === ownerData._id
-  );
+  const sortFeed = () => {
+    setHomeFeedSort(
+      homeFeedPosts?.slice().sort((postA, postB) => {
+        switch (sort) {
+          case "trending":
+            return postB?.likes?.likeCount - postA?.likes?.likeCount;
+
+          case "older":
+            return (
+              Number(
+                postA?.createdAt.substring(0, 9).split("-").join("") +
+                  postA?.createdAt.substring(11, 19).split(":").join("")
+              ) -
+              Number(
+                postB?.createdAt.substring(0, 9).split("-").join("") +
+                  postB?.createdAt.substring(11, 19).split(":").join("")
+              )
+            );
+
+          case "recent":
+            return (
+              Number(
+                postB?.createdAt.substring(0, 9).split("-").join("") +
+                  postB?.createdAt.substring(11, 19).split(":").join("")
+              ) -
+              Number(
+                postA?.createdAt.substring(0, 9).split("-").join("") +
+                  postA?.createdAt.substring(11, 19).split(":").join("")
+              )
+            );
+        }
+      })
+    );
+  };
 
   return (
     <>
-      <Filter />
+      <Filter sort={sort} setSort={setSort} />
       <NewPostCard />
       <div>
-        {filteredHomeFeed.map((post) => {
-          return <PostCard key={post.id} postData={post} />;
-        })}
+        {homeFeedSort
+          ?.filter(
+            (post) =>
+              ownerData?.following.some((user) => user._id === post.user_id) ||
+              post.user_id === ownerData._id
+          )
+          .map((post) => {
+            return <PostCard key={post.id} postData={post} />;
+          })}
       </div>
     </>
   );
